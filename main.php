7e8aab461914f091c5ffc655ae542bfbe362bd67
tanjx <1033210915@qq.com>
2020-02-26 01:08:16 +0800

    yii 安装及其过程中遇到的问题“

---
+++ main.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'aliases' => [
+        '@bower' => dirname(dirname(__DIR__)) . '/node_modules',
+        '@npm'   => dirname(dirname(__DIR__)) . '/node_modules',
+    ],
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+    ],
+];
